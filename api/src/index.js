debce7eea9d5980e7c1a3d2e9c45054693f7c50d
s.yaylyaev <yaylyaevsafuan@gmail.com>
2021-11-09 22:39:19 +0300

    подключение БД

--- api/src/index.js
+++ api/src/index.js
@@ -1,15 +1,41 @@
-const express = require('express')
+const express = require('express');
+const mongoose = require('mongoose');
+const { port, host, db } = require('./configuration');
+const { connectDb } = require('./helpers/db');
+
 const app = express();
 
-console.log('PORT', process.env.PORT);
-const port = process.env.PORT;
-const host = process.env.HOST;
+const postSchema = new mongoose.Schema({
+    name: String
+});
+const Post = mongoose.model('Post', postSchema);
 
 app.get("/test", (req, res) => {
     res.send('Our api server is working correctly');
 });
 
-app.listen(port, () => {
-    console.log(`Started api server on port: ${port}`);
-    console.log(`On host ${host}`);
-});
+const startServer = () => {
+    app.listen(port, () => {
+        console.log(`Started api server on port: ${port}`);
+        console.log(`On host ${host}`);
+        console.log(`Db ${db}`);
+
+        // Post.find(function (err, posts) {
+        //     if (err) return console.error(err);
+        //     console.log("posts", posts);
+        // })
+
+        const testPost = new Post({name: "TestPost"});
+        testPost.save(function (err, saveTestPost) {
+            if (err) return console.error(err);
+            console.log('savedTestPost', saveTestPost)
+
+        })
+    });
+}
+
+
+connectDb()
+    .on("error", console.log)
+    .on("disconnected", connectDb)
+    .once("open", startServer)
